ba30a9d9f0375d78a2e95fdd1ee0a906533396c3
hello-world00 <hello.world0@yandex.ru>
2019-10-22 21:23:26 +0500

    Added task 414.

---
+++ task_0414/main.cxx
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int N, k;
+    int A, B;
+    std::vector<int> arr;
+    std::vector<int> result;
+    std::cin >> N;
+    std::cin >> A >> B;
+    
+    for (int i = 0; i < N - 1;i++)
+    {
+        std::cin >> k;
+        arr.push_back(k);
+    }
+
+    for (int i = A - 2; i >= 0;)
+    {
+        if (arr[i] == B)
+        {
+            std::cout << B << std::endl;
+            return 0;
+        }
+        result.push_back(arr[i]);
+        i = arr[i] - 2;
+    }
+
+
+    for (int i = B - 2; i >= 0;)
+    {
+        if (arr[i] == A)
+        {
+            std::cout <<  A << std::endl;
+            return 0;
+        }
+
+        for (int j = 0; j < result.size(); j++)
+        {
+            
+            if (result[j] == arr[i])
+            {
+                std::cout << arr[i] << std::endl;
+                return 0;            
+            }
+
+        }
+        i = arr[i] - 2;
+    }
+
+    std::cout << 1 << std::endl;
+    return 0;
+}
